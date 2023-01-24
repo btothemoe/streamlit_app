4d4ddb47e13f654bff2c5da660e0b9a0c2426d97
Bryan Moore <93677399+btothemoe@users.noreply.github.com>
2023-01-23 16:52:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 # streamlit_app.py
 
 import streamlit as st
+import snowflake.connector
 
 # Everything is accessible via the st.secrets dict:
 
 st.write("DB username:", st.secrets["user"])
 st.write("DB account:", st.secrets["account"])
+
+
+#Connect to snowflake
+conn = snowflake.connector.connect(
+                user=st.secrets["user"],
+                password=st.secrets["password"],
+                account=st.secrets["account"],
+                warehouse=st.secrets["warehouse"],
+                database=st.secrets["database"],
+                schema=st.secrets["schema"]
+                )
