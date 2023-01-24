501a8ffcdd0447c02add7acf20070d2a87f48d33
Bryan Moore <93677399+btothemoe@users.noreply.github.com>
2023-01-23 16:00:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,3 @@ def init_connection():
 
 conn = init_connection()
 
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT top 10 * FROM sales;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
