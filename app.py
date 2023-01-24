8ec673eea279f62e92446d26257642138c512b80
Bryan Moore <93677399+btothemoe@users.noreply.github.com>
2023-01-23 16:55:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ conn = snowflake.connector.connect(
                 schema=st.secrets["schema"]
                 )
 
-cs = ctx.cursor()
+cs = conn.cursor()
 try:
     cs.execute("SELECT current_version()")
     one_row = cs.fetchone()
     print(one_row[0])
 finally:
     cs.close()
-ctx.close()
+conn.close()
