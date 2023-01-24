82c41df9d5378cee2c6243de1f626f410db3b0dd
Bryan Moore <bryanallenmoe@gmail.com>
2023-01-23 19:39:08 -0800

    Altering layout

--- app.py
+++ app.py
@@ -3,6 +3,28 @@
 import streamlit as st
 import snowflake.connector
 
+
+st.set_page_config(layout="wide")
+
+# Sidebar
+with st.sidebar:
+    st.title('INPUTS')
+    st.text_input('6-DIGIT SKU')
+    st.selectbox('COUNTRY', ['US', 'CA'])
+    st.button('RUN IT!')
+
+#Main Section
+st.title('WEB INVENTORY LOOKUP TOOL')
+st.write('When you’re wondering why a SKU isn’t showing up online, this tool has your back!')
+
+col1, col2 = st.columns((1,1))
+
+with col1:
+    st.header('ITEM INFO')
+
+with col2:
+    st.header('TESTS')
+
 # Everything is accessible via the st.secrets dict:
 
 st.write("DB username:", st.secrets["user"])
