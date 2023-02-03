eef47f71b3aba719033f6ce633d52da927fde6e5
Bryan Moore <bryanallenmoe@gmail.com>
2023-02-03 10:52:03 -0800

    Modified manager page with 081 connection

--- pages/manager.py
+++ pages/manager.py
@@ -2,6 +2,7 @@
 
 import streamlit as st
 import snowflake.connector
+import pyodbc
 
 st.set_page_config(
     layout="wide",
@@ -33,93 +34,27 @@ with st.sidebar:
 # APROPOS LOOKUP
 if sku:
 
-    st.header('APROPOS LOOKUP')
+    st.header('ZMRSQL081 LOOKUP')
     
-    #Connect to snowflake
-    conn = snowflake.connector.connect(
-                user=st.secrets["user"],
-                password=st.secrets["password"],
-                account=st.secrets["account"],
-                warehouse=st.secrets["warehouse"],
-                database=st.secrets["database"],
-                schema=st.secrets["schema"]
-                )
+    #Connect to ZMRSQL081
+    @st.experimental_singleton
+    def init_connection():
+        return pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+            + st.secrets["sql_server"]
+            + ";DATABASE="
+            + st.secrets["sql_database"]
+            + ";UID="
+            + st.secrets["sql_username"]
+            + ";PWD="
+            + st.secrets["sql_password"]
+        )
 
-    rows = run_query(f"""SELECT 
-        inv_id3				AS ItemId    
-        , TRIM(inv_desc)		AS ItemDesc
-        , TRIM(inv_id2)			AS CategoryId
-        ,CASE
-            WHEN LENGTH(TRIM(inv_subcategory)) = 0 THEN NULL::CHAR(4)
-            ELSE TRIM(inv_subcategory)
-        END					AS SubCatId
-        , TRIM(inv_id1)			AS VendorId
-        , TRIM(inv_ven_id)		AS VendorStyle
-        , inv_first_purch		AS FirstRecvdDate
-        , inv_last_pur			AS LastRecvdDate
-        , inv_cost				AS Cost
-        , inv_reg_price			AS OrigPrice
-        , inv_price				AS CurrPrice
-        , inv_wholesale2		AS SpecialPrice
-        , inv_wholesale1		AS TicketPrice
-        , TRIM(inv_del_flag)	AS atbStatus
-        , inv_user2				AS SubLicense
-    FROM inv
-    WHERE inv_id3 = '{sku}'""")
+    conn = init_connection()
 
+    rows = run_query("SELECT TOP 10 * FROM ZUMZ_ItemMaster;")
+    
     # Print results.
     st.dataframe(rows)
 
-    conn.close()
-
-
-# ZMRSQL081 LOOKUPS
-if sku:
-    
-    st.header('MASTERINTERFACE LOOKUP')
-    
-    #Connect to snowflake
-    conn = snowflake.connector.connect(
-                user=st.secrets["user"],
-                password=st.secrets["password"],
-                account=st.secrets["account"],
-                warehouse=st.secrets["warehouse"],
-                database=st.secrets["database"],
-                schema="ZUMZ_MI_US"
-                )
-
-    rows = run_query(f"""SELECT *
-	FROM ZUMZ_ItemMaster 
-	WHERE ItemId = '{sku}'""")
-
-    # Print results.
-    st.dataframe(rows)
-    
-    conn.close()
-
-
-
-# SALESWARP LOOKUP
-if sku:
-
-    st.header('SALESWARP LOOKUP')
-    
-    #Connect to snowflake
-    conn = snowflake.connector.connect(
-                user=st.secrets["user"],
-                password=st.secrets["password"],
-                account=st.secrets["account"],
-                warehouse=st.secrets["warehouse"],
-                database=st.secrets["database"],
-                schema="ZUMZ_SW4X_US"
-                )
-
-    rows = run_query(f"""SELECT *
-	FROM base_products
-	WHERE sku = '{sku}'
-    OR LEFT(sku, 6) = '{sku}'""")
-
-    # Print results.
-    st.dataframe(rows)
-    
     conn.close()
