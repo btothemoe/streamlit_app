813d73d417dd102b03b360c4d852bb699fda0d27
Bryan Moore <bryanallenmoe@gmail.com>
2023-06-16 08:37:11 -0700

    Updating to vertex tool

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# streamlit_app.py
-
 import streamlit as st
+import pandas as pd
 import snowflake.connector
 
 st.set_page_config(
     layout="wide",
-    page_title="Presale Tool",
+    page_title="Presale Manager",
 )
 
 
@@ -13,10 +12,42 @@ st.set_page_config(
 st.title('PRESALE MANAGER')
 st.write('When you’re wondering why a SKU isn’t showing up online, this tool has your back!')
 
-col1, col2 = st.columns((1,1))
+
+# Perform query function.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
 
+#Connect to snowflake
+conn = snowflake.connector.connect(
+            user=st.secrets["user"],
+            password=st.secrets["password"],
+            account=st.secrets["account"],
+            warehouse=st.secrets["warehouse"],
+            database=st.secrets["database"]
+)
+
+rows = run_query(f"""
+    SELECT 
+            SKU as sku
+        ,   QTY_INITIAL as qty_initial
+        ,   QTY_REMOVED as qty_removed
+        ,   CREATED_UTC as created
+        ,   MODIFIED_UTC as modified 
+    FROM ZUMZ_MI_US.ZUMZ_INVENTORY_PRESALE""")
+
+df = pd.DataFrame(
+    rows,
+    columns=('sku', 'qty_intitial', 'qty_removed', 'created', 'modified')
+)
 
 
+# Print results.
+st.dataframe(df)
 
 
+conn.close()
