1ee8bf894fd46e88e868ed5f79e7199299f1ca10
Bryan Moore <bryanallenmoe@gmail.com>
2023-06-20 11:17:47 -0700

    Added web query

--- app.py
+++ app.py
@@ -85,7 +85,59 @@ if (runReport):
     WHERE sl_date >= '{startDate}' -- CURRENT_DATE - 2
         AND sl_date <= '{endDate}'
         AND (atb.sl_2slsman != '600' OR atb.sl_register != 'OMS')
-        """)
+    
+    UNION ALL 
+
+    //Master Query - Web
+    SELECT DISTINCT
+        'DISTRIBUTE_TAX'                AS "Message Type"
+    ,   'sale'                          AS "Transaction Type"
+    ,   orders.customer_order_code      AS "Document Number"
+    ,   atb.sl_tran                     AS "Line Item Number"
+    ,   orders.order_date               AS "Document Date"
+    ,   atb.sl_date                     AS "Posting Date"
+    ,   'ZUMZ'                          AS "Company Code"
+    ,   'US'                            AS "Division Code"
+    ,   ''                              AS "Customer Class Code"            --Empty For Now
+    ,   atb.sl_2slsman                  AS "Admin Origin Location Code"
+    ,   atb.sl_store                    AS "Physical Origin Location Code"
+    ,   ''                              AS "Destination Location Code"      --Empty For Now
+    ,   shipaddress.address_1           AS "Destination Street Address" 
+    ,   shipaddress.locality            AS "Destination City"
+    ,   shipaddress.region              AS "Destination Main Division"
+    ,   shipaddress.postal_code         AS "Destination Postal Code"
+    ,   shipaddress.country_code        AS "Destination Country"
+    ,   CONCAT(LPAD(atb.sl_seq, 6, 0), 
+            LPAD(atb.sl_color, 4, 0), 
+            LPAD(atb.sl_size, 4, 0))    AS "Product Code"
+    ,   mi.VertexTaxCodeId              AS "Product Class"
+    ,   atb.sl_qty                      AS "Quantity"
+    ,   atb.sl_price                    AS "Extended Price"
+    ,   sl_taxamt1 
+            + sl_taxamt2 
+            + sl_taxamt3 
+            + sl_taxamt4                AS "Input Tax Total"
+    ,   orders.order_date               AS "Flex Date 1"
+    FROM raw.ZUMZ_ATB_US.sales                                      atb
+    JOIN Apollo.ZUMZ_US.SW_SALES_IMPORT_FILE                        sw
+        ON  atb.sl_cust      = sw.tl_cust
+        AND atb.sl_register  = sw.tl_register
+        AND atb.sl_trantype  = sw.tl_trantype
+        AND atb.sl_store     = sw.tl_store
+        AND atb.sl_date      = sw.tl_date
+        AND atb.sl_slsman    = sw.tl_slsman
+        AND atb.sl_inv_num   = sw.tl_inv_num
+        AND TRIM(atb.sl_eitem) = TRIM(sw.tl_eitem)::varchar(15)
+    JOIN raw.ZUMZ_MI_US.ZUMZ_ItemMaster                             mi
+        ON atb.sl_seq = mi.ItemId
+    JOIN raw.ZUMZ_SW4x_US.base_orders                               orders
+        ON sw.baseorder_id = orders.id
+    LEFT JOIN raw.ZUMZ_SW4x_US.customer_addresses                   shipaddress
+        ON orders.shipping_address_id = shipaddress.id
+    WHERE sl_date >= '{startDate}' -- CURRENT_DATE - 2
+        AND sl_date <= '{endDate}'
+    AND (sl_2slsman = '600' OR sl_register = 'OMS')  
+    """)
 
     pd.set_option('display.min_rows', 100)
     
